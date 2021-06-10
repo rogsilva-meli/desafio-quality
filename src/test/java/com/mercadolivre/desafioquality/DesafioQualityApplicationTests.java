59b5412cbd6a575b8916637506040514cd3739f5
Rogerio Ferreira da Silva <rogerio.silva@mercadolivre.com>
2021-06-10 16:01:34 -0300

    Primeiro commit

---
+++ src/test/java/com/mercadolivre/desafioquality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.desafioquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
